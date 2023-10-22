af98cb7b9d6619bd9841e5e382291528149894b3
Artem Safin <artemsafin.webdev@gmail.com>
2023-10-22 17:12:18 +0300

    ПО ренджам

--- src/js/rangeSliders.ts
+++ src/js/rangeSliders.ts
@@ -2,103 +2,107 @@ import noUiSlider from "nouislider";
 import "nouislider/dist/nouislider.css";
 
 export default function rangeSliders() {
-  const elements = Array.from(
-    document.querySelectorAll(".js-range-slider-double")
-  );
-
-  elements.forEach((element) => {
-    const inputs = Array.from(
-      element.querySelectorAll<HTMLInputElement>("input")
+  function init() {
+    const elements = Array.from(
+      document.querySelectorAll(".js-range-slider-double")
     );
-    const rangeSliderElement = element.querySelector<HTMLElement>(
-      ".js-range-slider-element"
-    );
-    if (!rangeSliderElement) return;
-    const minValue = element.hasAttribute("data-min")
-      ? Number(element.getAttribute("data-min"))
-      : 10;
-    const maxValue = element.hasAttribute("data-max")
-      ? Number(element.getAttribute("data-max"))
-      : 15;
-    const stepValue = element.hasAttribute("data-step")
-      ? Number(element.getAttribute("data-step"))
-      : 1;
-    const form = element.closest("form");
 
-    const startValue = inputs[0].value.replace(/\s/g, "").trim()
-      ? parseFloat(inputs[0].value.replace(/\s/g, "").trim())
-      : "";
-    const endValue = inputs[1].value.replace(/\s/g, "").trim()
-      ? parseFloat(inputs[1].value.replace(/\s/g, "").trim())
-      : "";
-
-    console.log("Range slider created");
-
-    noUiSlider.create(rangeSliderElement, {
-      start: [
-        startValue ? startValue : minValue,
-        endValue ? endValue : maxValue,
-      ],
-      connect: true,
-      orientation: "horizontal",
-      step: stepValue,
-      range: {
-        min: minValue,
-        max: maxValue,
-      },
-      format: {
-        to: (v) =>
-          Number(parseFloat(v.toString()).toFixed(0)).toLocaleString("ru-RU", {
-            useGrouping: true,
-          }),
-        from: (v) => Number(parseFloat(v).toFixed(0)),
-      },
-    });
-
-    // rangeSliderElement.noUiSlider.on('update', () => {
-    //     input.value = rangeSliderElement.noUiSlider.get();
-    // });
-
-    //@ts-ignore
-    rangeSliderElement.noUiSlider.on("update", () => {
-      //@ts-ignore
-      const newValue = rangeSliderElement.noUiSlider.get();
-      inputs[0].value = newValue[0];
-      inputs[1].value = newValue[1];
-    });
-
-    inputs[0].addEventListener("change", (event) => {
-      //@ts-ignore
-      rangeSliderElement.noUiSlider.set(
-        //@ts-ignore
-        event.target.value.replace(/[^\d]+/g, "")
+    elements.forEach((element) => {
+      const inputs = Array.from(
+        element.querySelectorAll<HTMLInputElement>("input")
       );
-    });
-    inputs[1].addEventListener("change", (event) => {
-      //@ts-ignore
-      rangeSliderElement.noUiSlider.set([
-        null,
-        //@ts-ignore
-        event.target.value.replace(/[^\d]+/g, ""),
-      ]);
-    });
+      const rangeSliderElement = element.querySelector<HTMLElement>(
+        ".js-range-slider-element"
+      );
+      if (!rangeSliderElement) return;
+      const minValue = element.hasAttribute("data-min")
+        ? Number(element.getAttribute("data-min"))
+        : 10;
+      const maxValue = element.hasAttribute("data-max")
+        ? Number(element.getAttribute("data-max"))
+        : 15;
+      const stepValue = element.hasAttribute("data-step")
+        ? Number(element.getAttribute("data-step"))
+        : 1;
+      const form = element.closest("form");
 
-    if (form) {
-      form.addEventListener("reset", () => {
-        console.log("Parent form has been reset");
-        //@ts-ignore
-        rangeSliderElement.noUiSlider.reset();
-        setTimeout(() => {
-          //@ts-ignore
-          const newValue = rangeSliderElement.noUiSlider.get();
-          inputs[0].value = newValue[0];
-          inputs[1].value = newValue[1];
-        }, 10);
+      const startValue = inputs[0].value.replace(/\s/g, "").trim()
+        ? parseFloat(inputs[0].value.replace(/\s/g, "").trim())
+        : "";
+      const endValue = inputs[1].value.replace(/\s/g, "").trim()
+        ? parseFloat(inputs[1].value.replace(/\s/g, "").trim())
+        : "";
 
-        // rangeSliderElement.noUiSlider.set([minValue, maxValue]);
+      console.log("Range slider created");
+
+      noUiSlider.create(rangeSliderElement, {
+        start: [
+          startValue ? startValue : minValue,
+          endValue ? endValue : maxValue,
+        ],
+        connect: true,
+        orientation: "horizontal",
+        step: stepValue,
+        range: {
+          min: minValue,
+          max: maxValue,
+        },
+        format: {
+          to: (v) => Number(parseFloat(v.toString()).toFixed(0)),
+          from: (v) => Number(parseFloat(v).toFixed(0)),
+        },
       });
-    }
-  });
+
+      // rangeSliderElement.noUiSlider.on('update', () => {
+      //     input.value = rangeSliderElement.noUiSlider.get();
+      // });
+
+      //@ts-ignore
+      rangeSliderElement.noUiSlider.on("update", () => {
+        //@ts-ignore
+        const newValue = rangeSliderElement.noUiSlider.get();
+        inputs[0].value = newValue[0];
+        inputs[1].value = newValue[1];
+      });
+
+      inputs[0].addEventListener("change", (event) => {
+        //@ts-ignore
+        rangeSliderElement.noUiSlider.set(
+          //@ts-ignore
+          event.target.value.replace(/[^\d]+/g, "")
+        );
+      });
+      inputs[1].addEventListener("change", (event) => {
+        //@ts-ignore
+        rangeSliderElement.noUiSlider.set([
+          null,
+          //@ts-ignore
+          event.target.value.replace(/[^\d]+/g, ""),
+        ]);
+      });
+
+      if (form) {
+        form.addEventListener("reset", () => {
+          console.log("Parent form has been reset");
+          //@ts-ignore
+          rangeSliderElement.noUiSlider.reset();
+          setTimeout(() => {
+            //@ts-ignore
+            const newValue = rangeSliderElement.noUiSlider.get();
+            inputs[0].value = newValue[0];
+            inputs[1].value = newValue[1];
+          }, 10);
+
+          // rangeSliderElement.noUiSlider.set([minValue, maxValue]);
+        });
+      }
+    });
+  }
+
+  init();
+
+  //@ts-ignore
+  window.initRanges = init;
 
   document.addEventListener("click", (event) => {
     const target = event.target as HTMLElement;
